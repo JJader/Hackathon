a9208183e7e9cb82636268eddfc2cda59e05d168
tuktuks <pedro.lucas.tukinha25@gmail.com>
2020-11-01 10:17:38 -0300

    incremento do backend

--- src/data/Projetos.js
+++ src/data/Projetos.js
@@ -1,93 +1,168 @@
 const Projetos = [
   {
-    "bolsa": 0,
-    "area": [
-      "Robótica",
-      "Programação",
-      "Eletrônica"
-    ],
-    "preRequisitos": [
-      "Programação de Computadores I",
-      "Princípios de Eletrônica Digital"
-    ],
-    "_id": "5f9cc9c1cba0e752640cf14b",
-    "id": 1,
-    "nome": "Segue Linha",
-    "tipo": "Projeto de núcleo",
-    "responsavel": "LocoBots",
-    "horas": 10,
-    "descricao": "Lorem Ipsum is simply dummy text of the printing and typesetting industry 1998. Lorem Ipsum has been the industry's 24.000 standard dummy text ever since the 1500s, when an unknown printer took a galley of type and scrambled it to make a type specimen book. It has survived not only five centuries, but also the leap into electronic typesetting, remaining essentially unchanged. It was popularised in the 1960s with the release of Letraset sheets containing Lorem",
-    "colaboradores": [
-      {
-        "_id": "5f9cc9c1cba0e752640cf14c",
-        "nome": "Lucas Novais",
-        "matricula": "18.1.8046"
-      },
-      {
-        "_id": "5f9cc9c1cba0e752640cf14d",
-        "nome": "Thiago Silva",
-        "matricula": "18.1.8017"
-      }
-    ],
-    "__v": 0
+      "status": "Em andamento",
+      "bolsa": 0,
+      "area": [
+          "Robótica",
+          "Programação",
+          "Eletrônica"
+      ],
+      "preRequisitos": [
+          "Matricula ICEA",
+          "Prog1"
+      ],
+      "horas": 0,
+      "id": "5f9d8f9d559111d05a444a2c",
+      "nome": "Robô segue linha",
+      "tipo": "Projeto do Núcleo de Robótica",
+      "responsavel":"LocoBots",
+      "descricao": "Esse projeto visa desenvolver um robô para competições do tipo segue linha. Será desenvolvido hardware e programação do robô.",
+      "colaboradores": [
+          {
+              "_id": "5f9d93075101191490835217",
+              "nome": "Thiago Silva Figueiredo",
+              "matricula": "18.1.8017"
+          },
+          {
+              "_id": "5f9d93075101191490835218",
+              "nome": "Pedro Lucas",
+              "matricula": "15.2.8008"
+          }
+      ],
+      "__v": 0
   },
   {
-    "bolsa": 300,
-    "area": [
-      "Arduíno",
-      "Programação",
-      "Eletrônica"
-    ],
-    "preRequisitos": [
-      "Programação de Computadores I",
-      "Princípios de Eletrônica Digital",
-      "Cálculo III"
-    ],
-    "_id": "5f9cca87cba0e752640cf14e",
-    "id": 2,
-    "nome": "Sumô",
-    "tipo": "Projeto de núcleo",
-    "responsavel": "LocoBots",
-    "horas": 40,
-    "descricao": "Criar um robô que segue luta sumô contra outros robôs que também.",
-    "colaboradores": [
-      {
-        "_id": "5f9cca87cba0e752640cf14f",
-        "nome": "Lucas Novais",
-        "matricula": "18.1.8046"
-      },
-      {
-        "_id": "5f9cca87cba0e752640cf150",
-        "nome": "Thiago Silva",
-        "matricula": "18.1.8017"
-      },
-      {
-        "_id": "5f9cca87cba0e752640cf151",
-        "nome": "Jamisson Jader Moraes Pereira Junior",
-        "matricula": "18.1.8060"
-      }
-    ],
-    "__v": 0
+      "status": "Em andamento",
+      "bolsa": 300,
+      "area": [
+          "Programação",
+          "Física"
+      ],
+      "preRequisitos": [
+          "Programação de Computadores I"
+      ],
+      "horas": 0,
+      "id": "5f9d8fae5101191490835214",
+      "nome": "Simulações 3D",
+      "tipo": "Proativa",
+      "responsavel": "LocoBots",
+      "descricao": "Projeto destinado ao desenvolvimento de simulações em 3D, nas quais possuem o intuito de melhorar a compreensão dos alunos da universidade que fazem disciplinas envolvendo mecânica clássica. Além disso, utiliza o sistema de detecção de movimentos Kinect para melhorar a integração dos alunos com o ambiente.",
+      "colaboradores": [
+          {
+              "_id": "5f9d93dd5101191490835219",
+              "nome": "Lucas Novais da Silva",
+              "matricula": "18.1.8046"
+          },
+          {
+              "_id": "5f9d93dd510119149083521a",
+              "nome": "Jamisson Junior",
+              "matricula": "18.1.8060"
+          }
+      ],
+      "__v": 0
   },
   {
-    "bolsa": 600,
-    "area": [
-      "Programação",
-      "Visão computacional"
-    ],
-    "preRequisitos": [
-      "Programação de Computadores I",
-      "A.E.D.S I"
-    ],
-    "_id": "5f9ccb12cba0e752640cf152",
-    "id": 3,
-    "nome": "Detecção de Borda",
-    "tipo": "Projeto de extensão",
-    "responsavel": "Alana",
-    "horas": 100,
-    "descricao": "Detectar bordas de qualquer coisa",
-    "colaboradores": [],
-    "__v": 0
+      "status": "Concluído",
+      "bolsa": 400,
+      "area": [
+          "Programação",
+          "Desenvolvimento Mobile"
+      ],
+      "preRequisitos": [
+          "Programação de Computadores II",
+          "Algoritmos e Estruturas de Dados I"
+      ],
+      "horas": 0,
+      "id": "5f9d90925101191490835215",
+      "nome": "Aplicativo de Ônibus",
+      "tipo": "Iniciação Científica",
+      "responsavel": "Rafael Alexandre",
+
+      "descricao": "O projeto consiste em desenvolver uma aplicação móvel que faz o rastreio da localização de ônibus ao redor da cidade.",
+      "colaboradores": [
+          {
+              "_id": "5f9d9452510119149083521b",
+              "nome": "Jamisson Junior",
+              "matricula": "18.1.8060"
+          }
+      ],
+      "__v": 0
+  },
+  {
+      "status": "Concluído",
+      "bolsa": 0,
+      "area": [
+          "Programação",
+          "Matemática",
+          "Cálculo",
+          "Visão Computacional"
+      ],
+      "preRequisitos": [
+          "Matricula ICEA",
+          "Cálculo1"
+      ],
+      "horas": 0,
+      "id": "5f9d9095559111d05a444a2d",
+      "nome": "Aplicação da Derivada no curso Eng da Computação",
+      "tipo": "Projeto de extensão",
+      "responsavel":"Alana Cavalcante",
+
+      "descricao": "Esse projeto visa estudar o processamento de imagens, mais especificamente a detecção de bordas proposta por John Canny, buscando demonstrar formas de se aplicar o cálculo diferencial como uma ferramenta nos algoritmos. A fim de potencializar o processamento de imagem, será desenvolvido um algoritmo capaz de utilizar as bordas de uma imagem para realizar medições de objetos em tempo real.",
+      "colaboradores": [
+          {
+              "_id": "5f9d94cb510119149083521c",
+              "nome": "Jamisson Junior",
+              "matricula": "18.1.8060"
+          },
+          {
+              "_id": "5f9d94cb510119149083521d",
+              "nome": "Thiago Figueiredo",
+              "matricula": "18.1.8017"
+          }
+      ],
+      "__v": 0
+  },
+  {
+      "status": "Novo",
+      "bolsa": 0,
+      "area": [
+          "Programação",
+          "Desenvolvimento de Jogos"
+      ],
+      "preRequisitos": [
+          "Programação de Computadores I"
+      ],
+      "horas": 0,
+      "id": "5f9d91445101191490835216",
+      "nome": "Monlewood Crackdown",
+      "tipo": "Projeto pessoal",
+      "responsavel":"Lucas Novais da Silva",
+
+      "descricao": "O projeto consiste em um jogo 2D de plataforma, em que o personagem principal precisa utilizar de armas improvisadas encontradas no cenário para derrotar os inimigos e prosseguir de fase. O jogo está sendo desenvolvido no Unity.",
+      "colaboradores": [],
+      "__v": 0
+  },
+  {
+      "status": "Novo",
+      "bolsa": 300,
+      "area": [
+          "Programação",
+          "Desenvolvimento Web",
+          "Marketing",
+          "Ciência de dados"
+      ],
+      "preRequisitos": [
+          "Matricula ICEA",
+          "Prog1"
+      ],
+      "horas": 15,
+      "id": "5f9d9147559111d05a444a2e",
+      "nome": "Desenvolvimento sistema Web Gerdau",
+      "tipo": "Projeto da Empresa Junior",
+      "responsavel": "Visão",
+      "descricao": "Neste projeto será desenvolvido um sistema Web para gerenciar dados da empresa Gerdau. Utilizar-se-a da Ciência de dados para levantamentos estatísticos da empresa.",
+      "colaboradores": [],
+      "__v": 0
   }
 ]
 
--- src/data/Usuarios.js
+++ src/data/Usuarios.js
@@ -1,63 +1,226 @@
 const Usuarios = [
   {
-    "areaInteresse": [
-      "Programação",
-      "Eletrônica"
-    ],
-    "_id": "5f9cc8143be3661620d8640a",
-    "nome": "Lucas Novais Ferreira",
-    "matricula": "18.1.8046",
-    "curso": "Engenharia de produxota",
-    "foto": '../../assets/brad.png',
-    "Projeto": [
-      {
-        "_id": "5f9cc8143be3661620d8640b",
-        "id": 1,
-        "nome": "Simulações"
-      },
-      {
-        "_id": "5f9cc8143be3661620d8640c",
-        "id": 2,
-        "nome": "Segue Linha"
-      }
-    ],
-    "horas": 100,
-    "__v": 0
+      "foto": "fotoPadrao.jpg",
+      "areaInteresse": [
+          "Robótica",
+          "IoT",
+          "Visão Computacional"
+      ],
+      "horas": 0,
+      "_id": "5f9d8cb3559111d05a444a27",
+      "nome": "Thiago Figueiredo",
+      "matricula": "18.1.8017",
+      "curso": "Engenharia da Computação",
+      "Projeto": [
+          {
+              "_id": "5f9d97271d13fc54f0df45b1",
+              "id": "5f9d8f9d559111d05a444a2c",
+              "nome": "Robô segue linha"
+          },
+          {
+              "_id": "5f9d97271d13fc54f0df45b2",
+              "id": "5f9d9095559111d05a444a2d",
+              "nome": "Aplicação da derivada no curso Eng da Computação"
+          }
+      ],
+      "__v": 0
   },
   {
-    "areaInteresse": [
-      "Programação",
-      "Eletrônica",
-      "Visao computacional"
-    ],
-    "_id": "5f9ccb917d59ef5518665fbd",
-    "nome": "Carlos",
-    "matricula": "18.1.9999",
-    "curso": "Sistemas de Informação",
-    "foto": "../../assets/user.png",
-    "Projeto": [
-      {
-        "_id": "5f9ccb917d59ef5518665fbe",
-        "id": 1,
-        "nome": "Simulações"
-      }
-    ],
-    "horas": 200,
-    "__v": 0
+      "foto": "fotoPadrao.jpg",
+      "areaInteresse": [
+          "Robótica",
+          "Eletrônica",
+          "Hardware",
+          "Programação"
+      ],
+      "horas": 0,
+      "_id": "5f9d8ce5559111d05a444a28",
+      "nome": "Jamisson Junior",
+      "matricula": "18.1.8060",
+      "curso": "Engenharia da Computação",
+      "Projeto": [
+          {
+              "_id": "5f9d985c1d13fc54f0df45b5",
+              "id": "5f9d8fae5101191490835214",
+              "nome": "Simulações 3D"
+          },
+          {
+              "_id": "5f9d985c1d13fc54f0df45b6",
+              "id": "5f9d90925101191490835215",
+              "nome": "Aplicativo de Ônibus"
+          },
+          {
+              "_id": "5f9d985c1d13fc54f0df45b7",
+              "id": "5f9d9095559111d05a444a2d",
+              "nome": "Aplicação da derivada no curso Eng da Computação"
+          }
+      ],
+      "__v": 0
   },
   {
-    "areaInteresse": [
-      "Cálculo",
-      "Robótica"
-    ],
-    "_id": "5f9ccbde7d59ef5518665fbf",
-    "nome": "Astobaldo",
-    "matricula": "18.1.9999",
-    "curso": "Engenharia Elétrica",
-    "foto": "myProfPic3.jpg",
-    "Projeto": [],
-    "horas": 400,
-    "__v": 0
+      "foto": "fotoPadrao.jpg",
+      "areaInteresse": [
+          "Programação",
+          "Física",
+          "Cálculo",
+          "Inteligência Artificial"
+      ],
+      "horas": 0,
+      "_id": "5f9d8ce5510119149083520f",
+      "nome": "Lucas Novais da Silva",
+      "matricula": "18.1.8046",
+      "curso": "Engenharia de Computação",
+      "Projeto": [
+          {
+              "_id": "5f9d97e51d13fc54f0df45b3",
+              "id": "5f9d8fae5101191490835214",
+              "nome": "Simulações 3D"
+          },
+          {
+              "_id": "5f9d97e51d13fc54f0df45b4",
+              "id": "5f9d91445101191490835216",
+              "nome": "Monlewood Crackdown"
+          }
+      ],
+      "__v": 0
+  },
+  {
+      "foto": "fotoPadrao.jpg",
+      "areaInteresse": [
+          "Marketing",
+          "Programação",
+          "Engenharia de Software"
+      ],
+      "horas": 0,
+      "_id": "5f9d8d3b559111d05a444a29",
+      "nome": "Alexander Marx",
+      "matricula": "18.1.8018",
+      "curso": "Engenharia da Computação",
+      "Projeto": [],
+      "__v": 0
+  },
+  {
+      "foto": "fotoPadrao.jpg",
+      "areaInteresse": [
+          "Banco de dados",
+          "Inteligencia Artificial",
+          "Robótica"
+      ],
+      "horas": 0,
+      "_id": "5f9d8d68559111d05a444a2a",
+      "nome": "Pedro Lucas",
+      "matricula": "15.2.8008",
+      "curso": "Engenharia da Produção",
+      "Projeto": [
+          {
+              "_id": "5f9d98b11d13fc54f0df45b8",
+              "id": "5f9d8f9d559111d05a444a2c",
+              "nome": "Robô segue linha"
+          }
+      ],
+      "__v": 0
+  },
+  {
+      "foto": "fotoPadrao.jpg",
+      "areaInteresse": [
+          "Eletrônica",
+          "Robótica",
+          "Programação"
+      ],
+      "horas": 0,
+      "_id": "5f9d8d6b5101191490835210",
+      "nome": "LocoBots",
+      "matricula": "ndr-1000",
+      "curso": "Engenharia Elétrica",
+      "Projeto": [
+          {
+              "_id": "5f9d99531d13fc54f0df45b9",
+              "id": "5f9d8f9d559111d05a444a2c",
+              "nome": "Robô segue linha"
+          },
+          {
+              "_id": "5f9d99531d13fc54f0df45ba",
+              "id": "5f9d8fae5101191490835214",
+              "nome": "Simulações 3D"
+          }
+      ],
+      "__v": 0
+  },
+  {
+      "foto": "fotoPadrao.jpg",
+      "areaInteresse": [
+          "Matemática",
+          "Programação"
+      ],
+      "horas": 0,
+      "_id": "5f9d8db55101191490835211",
+      "nome": "Alana Cavalcante",
+      "matricula": "prof-2000",
+      "curso": "Engenharia Elétrica",
+      "Projeto": [
+          {
+              "_id": "5f9d99c21d13fc54f0df45bc",
+              "id": "5f9d9095559111d05a444a2d",
+              "nome": "Aplicação da derivada no curso Eng da Computação"
+          }
+      ],
+      "__v": 0
+  },
+  {
+      "foto": "fotoPadrao.jpg",
+      "areaInteresse": [
+          "Desenvolvimento Web",
+          "Inteligencia Artificial",
+          "Ciência de dados"
+      ],
+      "horas": 0,
+      "_id": "5f9d8ddb559111d05a444a2b",
+      "nome": "Ana Beatriz",
+      "matricula": "17.1.8010",
+      "curso": "Sistemas de Informação",
+      "Projeto": [],
+      "__v": 0
+  },
+  {
+      "foto": "fotoPadrao.jpg",
+      "areaInteresse": [
+          "Inteligência Artificial",
+          "Programação",
+          "Desenvolvimento Mobile"
+      ],
+      "horas": 0,
+      "_id": "5f9d8de45101191490835212",
+      "nome": "Rafael Alexandre",
+      "matricula": "prof-3000",
+      "curso": "Engenharia de Computação",
+      "Projeto": [
+          {
+              "_id": "5f9d99f11d13fc54f0df45bd",
+              "id": "5f9d90925101191490835215",
+              "nome": "Aplicativo de Ônibus"
+          }
+      ],
+      "__v": 0
+  },
+  {
+      "foto": "fotoPadrao.jpg",
+      "areaInteresse": [
+          "Programação",
+          "Desenvolvimento Web"
+      ],
+      "horas": 0,
+      "_id": "5f9d8e245101191490835213",
+      "nome": "Visao",
+      "matricula": "ej-4000",
+      "curso": "Sistemas de informação",
+      "Projeto": [
+          {
+              "_id": "5f9d9b2f1d13fc54f0df45c4",
+              "id": "5f9d9147559111d05a444a2e",
+              "nome": "Desenvolvimento de sistema Web Gerdau"
+          }
+      ],
+      "__v": 0
   }
 ]
 
